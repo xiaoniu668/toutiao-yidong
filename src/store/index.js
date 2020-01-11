d89b17bcec4c3e737028b3e94555696c0b13b1ae
unknown <wangjunyang@look.com>
2020-01-11 22:50:16 +0800

    登录页面-本地存储 token优化封装

--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,23 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
+
+const USER_KEY = 'user'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     // 当前登录用户状态  token
-    user: JSON.parse(window.localStorage.getItem('user'))
+    // user: JSON.parse(window.localStorage.getItem('user'))
+    user: getItem(USER_KEY)
   },
   mutations: {
     setUser (state, data) {
       state.user = data
       // 为了避免刷新页面数据丢失  在这里对使用本地存储   进行持久化
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      // window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem(USER_KEY, state.user)
     }
   },
   actions: {
