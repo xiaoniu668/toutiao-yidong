3a0ed05ad119ac3839895650521a4976289aa5ad
unknown <wangjunyang@look.com>
2020-01-12 16:04:04 +0800

    登录界面-发送token

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,9 +2,15 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
 
+import store from '@/store'
+
+// axios.create 方法创建一个和axios 本身功能一样的一个对象
 const request = axios.create({
   baseURL: ' http://ttapi.research.itcast.cn/'
 })
+
+// axios 开放了自定义转换后端返回的  api
+// data 就是后端返回的原始数据
 request.defaults.transformResponse = [function (data) {
   try {
     return jsonBig.parse(data)
@@ -14,4 +20,28 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+
+// 请求拦截器
+
+axios.interceptors.request.use(function (config) {
+  // config 请求配置对象 可以修改config来实现同意请求数据处理
+  const { user } = store.state
+
+  // 统一添加token
+  if (user) {
+    // config.headers.获取操作请求头对象
+    config.headers.Authorization = `Bearer ${user.tokken}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+
+axios.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
 export default request
